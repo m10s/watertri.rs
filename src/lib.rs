69458aab005115031cda5cc3cf18c9bf58836638
Michael Weiss <michael.weiss@m10s.at>
2019-05-06 21:50:40 +0200

    Generify

--- src/lib.rs
+++ src/lib.rs
@@ -8,41 +8,53 @@
 // Variable names from the paper (appendix A) are not snake_case
 extern crate cgmath;
 
-use cgmath::Vector3;
+// use cgmath::num_traits::Signed;
+use cgmath::{BaseFloat, Vector3};
 
 /// Precomputed data depending only on the ray.
 #[derive(Clone, Debug)]
-pub struct RayData {
+pub struct RayData<S> {
     kx: usize,
     ky: usize,
     kz: usize,
-    Sx: f32,
-    Sy: f32,
-    Sz: f32,
-    org: Vector3<f32>,
+    Sx: S,
+    Sy: S,
+    Sz: S,
+    org: Vector3<S>,
 }
 
-impl RayData {
+impl<S> RayData<S>
+where
+    S: BaseFloat,
+{
     /// Pre-compute the transformation that is applied to all triangles.
-    pub fn new(org: Vector3<f32>, dir: Vector3<f32>) -> RayData {
+    pub fn new(org: Vector3<S>, dir: Vector3<S>) -> RayData<S> {
         // The paper swaps kx and ky if dir[kz] is negative, to preserve winding order.
         // But winding order is only relevant for backface culling, which we don't perform.
         let kz = max_dim(dir);
         let kx = (kz + 1) % 3;
         let ky = (kz + 2) % 3;
+
+        // S::div(1.0, dir[kz]);
+
         RayData {
             kx: kx,
             ky: ky,
             kz: kz,
             Sx: dir[kx] / dir[kz],
             Sy: dir[ky] / dir[kz],
-            Sz: 1.0 / dir[kz],
+            Sz: S::one() / dir[kz],
             org: org,
         }
     }
 
     /// Perform the intersection calculation.
-    pub fn intersect(&self, A: Vector3<f32>, B: Vector3<f32>, C: Vector3<f32>) -> Option<Intersection> {
+    pub fn intersect(
+        &self,
+        A: Vector3<S>,
+        B: Vector3<S>,
+        C: Vector3<S>,
+    ) -> Option<Intersection<S>> {
         let (Sx, Sy, Sz, org) = (self.Sx, self.Sy, self.Sz, self.org);
         let (kx, ky, kz) = (self.kx, self.ky, self.kz);
         let (A, B, C) = (A - org, B - org, C - org);
@@ -57,24 +69,26 @@ impl RayData {
         let mut V = Ax * Cy - Ay * Cx;
         let mut W = Bx * Ay - By * Ax;
 
-        if U == 0. || V == 0. || W == 0. {
-            let CxBy = (Cx as f64) * (By as f64);
-            let CyBx = (Cy as f64) * (Bx as f64);
-            U = (CxBy - CyBx) as f32;
-            let AxCy = (Ax as f64) * (Cy as f64);
-            let AyCx = (Ay as f64) * (Cx as f64);
-            V = (AxCy - AyCx) as f32;
-            let BxAy = (Bx as f64) * (Ay as f64);
-            let ByAx = (By as f64) * (Ax as f64);
-            W = (BxAy - ByAx) as f32;
+        if U == S::zero() || V == S::zero() || W == S::zero() {
+            let CxBy = Cx * By;
+            let CyBx = Cy * Bx;
+            U = CxBy - CyBx;
+            let AxCy = Ax * Cy;
+            let AyCx = Ay * Cx;
+            V = AxCy - AyCx;
+            let BxAy = Bx * Ay;
+            let ByAx = By * Ax;
+            W = BxAy - ByAx;
         }
 
-        if (U < 0. || V < 0. || W < 0.) && (U > 0. || V > 0. || W > 0.) {
+        if (U < S::zero() || V < S::zero() || W < S::zero())
+            && (U > S::zero() || V > S::zero() || W > S::zero())
+        {
             return None;
         }
 
         let det = U + V + W;
-        if det == 0. {
+        if det == S::zero() {
             return None;
         }
 
@@ -83,7 +97,7 @@ impl RayData {
         let Cz = Sz * C[kz];
         let T = U * Az + V * Bz + W * Cz;
 
-        let rcpDet = 1.0 / det;
+        let rcpDet = S::one() / det;
         Some(Intersection {
             t: T * rcpDet,
             u: U * rcpDet,
@@ -94,18 +108,21 @@ impl RayData {
 }
 
 /// Geometric information about a ray-triangle intersection.
-pub struct Intersection {
+pub struct Intersection<S> {
     /// Parametric distance from the ray origin to the intersection.
-    pub t: f32,
+    pub t: S,
     /// Barycentric coordinate.
-    pub u: f32,
+    pub u: S,
     /// Barycentric coordinate.
-    pub v: f32,
+    pub v: S,
     /// Barycentric coordinate.
-    pub w: f32,
+    pub w: S,
 }
 
-fn max_dim(v: Vector3<f32>) -> usize {
+fn max_dim<S>(v: Vector3<S>) -> usize
+where
+    S: BaseFloat,
+{
     let (x, y, z) = (v.x.abs(), v.y.abs(), v.z.abs());
     if x > y {
         // y isn't the maximum, so it's either x or z
